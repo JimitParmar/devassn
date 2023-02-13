14f57bbba576d11976db9bddef8a2005ac5708b8
JimitParmar <79106752+JimitParmar@users.noreply.github.com>
2023-02-13 10:14:41 +0530

    Update javaexp.java

--- javaexp.java
+++ javaexp.java
@@ -1,6 +1,6 @@
 class Cube {
     public static void main(String args[]) {
      int n=7;
-     console.log("The cube of the given number is:",system.out.printin(n*n*n))
+     console.log("The cube of the given number is:",system.out.printin(n*n*n));
     }
 }
